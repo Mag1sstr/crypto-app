f2d6e23baa219131ed8ede1379fa62267a7f98d9
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-01-21 05:32:15 +0500

    Update AppContent.jsx

--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -1,14 +1,34 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+import { useCrypto } from "../contexts/CryptoContext";
+import PortfolioChart from "./PortfolioChart";
+import AssetsTable from "./AssetsTable";
 
 const contentStyle = {
-  textAlign: "center",
+  // textAlign: "center",
   minHeight: "calc(100vh - 90px)",
   lineHeight: "120px",
-  color: "#fff",
-  backgroundColor: "#0958d9",
+  color: "#000",
+  backgroundColor: "#FFFAFA",
   padding: "1rem",
 };
 
 export default function AppContent() {
-  return <Layout.Content style={contentStyle}>Content</Layout.Content>;
+  const { assets, crypto } = useCrypto();
+  return (
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={3}>
+        Portfolio:{" "}
+        {assets
+          .map((asset) => {
+            const coin = crypto.find((c) => c.id === asset.id);
+            return asset.amount * coin.price;
+          })
+          .reduce((acc, v) => (acc += v), 0)
+          .toFixed(4)}
+        $
+      </Typography.Title>
+      <PortfolioChart />
+      <AssetsTable />
+    </Layout.Content>
+  );
 }
