5aa91eb48e93ef3935560c1a138bd6ecaf325d5f
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-01-26 17:29:02 +0500

    Update AssetsTable.jsx

--- src/components/AssetsTable.jsx
+++ src/components/AssetsTable.jsx
@@ -1,7 +1,49 @@
+import { Table } from "antd";
+import { useCrypto } from "../contexts/CryptoContext";
+import { capitalize } from "../utils/utils";
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    showSorterTooltip: {
+      target: "full-header",
+    },
+
+    sorter: (a, b) => a.name.length - b.name.length,
+    sortDirections: ["descend"],
+  },
+  {
+    title: "Price $",
+    dataIndex: "price",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: "Amount",
+    dataIndex: "amount",
+    sorter: (a, b) => a.amount - b.amount,
+  },
+];
+
 export default function AssetsTable() {
+  const { assets } = useCrypto();
+
+  const data = assets.map((a) => ({
+    key: a.id,
+    name: capitalize(a.id),
+    price: a.price,
+    amount: a.amount,
+  }));
   return (
     <section>
-      <p>Table</p>
+      <Table
+        pagination={false}
+        columns={columns}
+        dataSource={data}
+        showSorterTooltip={{
+          target: "sorter-icon",
+        }}
+      />
     </section>
   );
 }
