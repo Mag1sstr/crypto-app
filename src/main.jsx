f0a557db558fd55ec48c8af3acf0eb26e1442b3e
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-01-29 02:08:13 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import CryptoContextProvider from "./contexts/CryptoContext.jsx";
+import ThemeContextProvider from "./contexts/ThemeContext.jsx";
 
 const root = document.getElementById("root");
 
 createRoot(root).render(
   <CryptoContextProvider>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </CryptoContextProvider>
 );
